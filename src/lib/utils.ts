9167719729656a6273b051783cc0e2b4fb3d8a54
injal123 <162284560+injal123@users.noreply.github.com>
2026-01-03 19:36:21 +0545

    Add Clerk auth and light/dark mode toggle

---
+++ src/lib/utils.ts
@@ -0,0 +1,9 @@
+// through shadcn installation.
+
+
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
