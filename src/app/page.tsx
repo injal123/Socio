940d2800bd3c0f9c3689b09915062ade0c5cffff
injal123 <162284560+injal123@users.noreply.github.com>
2026-01-03 17:10:04 +0545

    Initial setup with Next.js

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+        <h1 className="text-4xl font-bold">Welcome to Socio!</h1>
+        <p className="mt-4 text-gray-600">Starting my social media app journey</p>
+    </div>
+  );
+}
